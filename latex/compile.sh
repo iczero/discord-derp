f3e483e985fce3d64974f8f45b13995f1f83f7a9
iczero <iczero4@gmail.com>
2020-11-22 19:09:44 -0800

    Add latex compile timeout

--- latex/compile.sh
+++ latex/compile.sh
@@ -2,5 +2,5 @@
 # This script is called in the container to do the actual compilation
 set -xe
 
-latex --no-shell-escape -interaction=batchmode input.tex
+timeout 15s latex --no-shell-escape -interaction=batchmode input.tex
 dvipng -bg Transparent -fg 'rgb 1.0 1.0 1.0' -D 192 input.dvi -o output.png
