baf6c6790a8ae7d6380498ed95777d0769329bf6
iczero <iczero4@gmail.com>
2021-02-05 22:36:52 -0800

    use ghostscript instead of dvipng

--- latex/compile.sh
+++ latex/compile.sh
@@ -2,5 +2,5 @@
 # This script is called in the container to do the actual compilation
 set -xe
 
-timeout 15s latex --no-shell-escape -interaction=batchmode input.tex
-dvipng -bg Transparent -fg 'rgb 1.0 1.0 1.0' -D 192 input.dvi -o output.png >> input.log 2>&1
+timeout 15s pdflatex --no-shell-escape -interaction=batchmode input.tex
+gs -dBATCH -dNOPAUSE -dSAFER -DTextAlphaBits=4 -dGraphicsOutputBits=4 -sDEVICE=pngalpha -r300 -o output.png input.pdf
